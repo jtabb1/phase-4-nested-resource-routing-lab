adad03599019f56a324edcfc2c22cce454f64e88
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-05 21:08:37 -0400

    Complete assignment.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    if params[:user]
-      user = User.find(params[:user_id])
+    if params[:user_id]
+      user = find_user
       items = user.items
     else
       items = Item.all
@@ -12,39 +12,31 @@ class ItemsController < ApplicationController
   end
 
   def show
-    item = Item.find(params[:id])
+    item = find_item
     render json: item
   end
-  
+
   def create
-    # if params[:user]
-    #   user = User.find(params[:user_id])
-    #   items = user.items
-    # else
-    #   items = Item.all
-    # end
-    # render json: items, include: :user
-    item = Item.create(item_params)
+    user = find_user
+    item = user.items.create(item_params)
     render json: item, status: :created
   end
-  
-  # def index
-  #   items = Item.all
-  #   render json: items, include: :user
-  # end
 
   private
 
-  def render_not_found_response
-    if params[:user]
-      render json: { error: "User not found" }, status: :not_found
-    else
-      render json: { error: "Item not found" }, status: :not_found
-    end
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
   end
 
   def item_params
     params.permit(:name, :description, :price)
   end
 
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
